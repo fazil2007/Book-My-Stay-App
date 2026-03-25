242406eaf9e685ca4bc07037f17357ac8765df3d
fazil2007 <mohamedfazil9944@gmail.com>
2026-03-25 10:41:10 +0530

    bx

--- App/src/BookMyStayApp.java
+++ App/src/BookMyStayApp.java
@@ -1,8 +1,16 @@
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
+
+class Reservation {
+    String guestName;
+    String roomType;
+
+    public Reservation(String guestName, String roomType) {
+        this.guestName = guestName;
+        this.roomType = roomType;
+    }
+}
 
 class RoomInventory {
-
     private Map<String, Integer> inventory;
 
     public RoomInventory() {
@@ -15,32 +23,50 @@ class RoomInventory {
     public int getAvailableRooms(String type) {
         return inventory.getOrDefault(type, 0);
     }
+
+    public void decrementRoom(String type) {
+        inventory.put(type, inventory.get(type) - 1);
+    }
 }
 
-public class BookMyStayApp {
+public class BookMyStayApp{
 
     public static void main(String[] args) {
 
+        Queue<Reservation> queue = new LinkedList<>();
+
+        queue.add(new Reservation("Abhi", "Single"));
+        queue.add(new Reservation("Subha", "Single"));
+        queue.add(new Reservation("Vanmathi", "Suite"));
+
         RoomInventory inventory = new RoomInventory();
 
-        System.out.println("Hotel Room Inventory Status\n");
+        Map<String, Integer> roomCounter = new HashMap<>();
+        roomCounter.put("Single", 0);
+        roomCounter.put("Double", 0);
+        roomCounter.put("Suite", 0);
 
-        System.out.println("Single Room:");
-        System.out.println("Beds: 1");
-        System.out.println("Size: 250 sqft");
-        System.out.println("Price per night: 1500.0");
-        System.out.println("Available Rooms: " + inventory.getAvailableRooms("Single") + "\n");
+        Set<String> allocatedRooms = new HashSet<>();
 
-        System.out.println("Double Room:");
-        System.out.println("Beds: 2");
-        System.out.println("Size: 400 sqft");
-        System.out.println("Price per night: 2500.0");
-        System.out.println("Available Rooms: " + inventory.getAvailableRooms("Double") + "\n");
+        System.out.println("Room Allocation Processing");
 
-        System.out.println("Suite Room:");
-        System.out.println("Beds: 3");
-        System.out.println("Size: 750 sqft");
-        System.out.println("Price per night: 5000.0");
-        System.out.println("Available Rooms: " + inventory.getAvailableRooms("Suite"));
+        while (!queue.isEmpty()) {
+            Reservation r = queue.poll();
+
+            if (inventory.getAvailableRooms(r.roomType) > 0) {
+
+                int count = roomCounter.get(r.roomType) + 1;
+                roomCounter.put(r.roomType, count);
+
+                String roomId = r.roomType + "-" + count;
+
+                if (!allocatedRooms.contains(roomId)) {
+                    allocatedRooms.add(roomId);
+                    inventory.decrementRoom(r.roomType);
+
+                    System.out.println("Booking confirmed for Guest: " + r.guestName + ", Room ID: " + roomId);
+                }
+            }
+        }
     }
 }
