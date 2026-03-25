50d672492190caf96ad51bb5be16daa701d0d6f7
fazil2007 <mohamedfazil9944@gmail.com>
2026-03-25 10:24:51 +0530

    UC-4:resloved error

--- App/src/BookMyStayApp.java
+++ App/src/BookMyStayApp.java
@@ -17,7 +17,7 @@ class RoomInventory {
     }
 }
 
-}public class BookMyStayApp {
+public class BookMyStayApp {
 
     public static void main(String[] args) {
 
@@ -43,3 +43,4 @@ class RoomInventory {
         System.out.println("Price per night: 5000.0");
         System.out.println("Available Rooms: " + inventory.getAvailableRooms("Suite"));
     }
+}
